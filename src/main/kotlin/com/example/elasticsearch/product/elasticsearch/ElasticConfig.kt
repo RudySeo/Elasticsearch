578b89498e58f57118a157d7be70bf537b9265bf
RudySeo <gun100491@gmail.com>
2025-10-12 23:16:23 +0900

    feat(ElasticConfig): ElasticConfig 설정 추가

--- src/main/kotlin/com/example/elasticsearch/product/elasticsearch/ElasticConfig.kt
+++ src/main/kotlin/com/example/elasticsearch/product/elasticsearch/ElasticConfig.kt
@@ -1,11 +1,22 @@
 package com.example.elasticsearch.product.elasticsearch
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.client.ClientConfiguration
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
 
-class ElasticConfig : ElasticsearchConfiguration() {
+@Configuration
+@EnableElasticsearchRepositories
+class ElasticConfig(
+    @Value("\${spring.data.elasticsearch.uris}") private val elasticUri: String,
+    @Value("\${spring.data.elasticsearch.username}") private val username: String,
+    @Value("\${spring.data.elasticsearch.password}") private val password: String
+) : ElasticsearchConfiguration() {
+
     override fun clientConfiguration(): ClientConfiguration =
         ClientConfiguration.builder()
-            .connectedTo("localhost:9200") //
+            .connectedTo(elasticUri)
+            .withBasicAuth(username, password)
             .build()
 }
